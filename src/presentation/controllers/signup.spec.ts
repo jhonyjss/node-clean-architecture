f88c45d462216398a6e211b28b4ec729a5035664
Jhony Souza <jhony.jss@gmail.com>
2020-07-21 00:01:27 -0300

    feat: ensure SignupController return 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -32,3 +32,19 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
+
+describe('Signup Controller', () => {
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_passwordConfirmation'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+})
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,10 @@
 import { HttpResponse, HttpRequest } from '../../protocols/http'
 import { MissingParamError } from '../errors/missing-param-erros'
-import { badRequest } from "../helpers/http-helper"
+import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
-  handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email'];
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
